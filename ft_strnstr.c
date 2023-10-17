36fa300529336d8abd0edc91e4bc1204f6b7156c
Ana Cavalcanti <anacavancalti@gmail.com>
2023-10-17 19:21:55 +0200

    updated strnstrn

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,31 +5,48 @@
 /*                                                     +:+                    */
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/10/09 18:11:27 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/10/17 14:29:42 by anacaval      ########   odam.nl         */
+/*   Created: 2023/10/08 10:39:29 by anacaval      #+#    #+#                 */
+/*   Updated: 2023/10/17 19:21:40 by anacavalcan   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-// #include <libft.h>
+// Cast to the address of a char in a string
+// locates a substring in a string
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len) 
+#include <libft.h>
+// #include <stdio.h>
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-char found;
-size_t i;
+    size_t i;
+    size_t j;
 
-i = 0;
-
+    i = 0;
+    j = 0;
+    while (needle[i] != '\0')
+    {   
+        while(haystack[j] && j <= len)
+        {
+            if(haystack[j] == needle[i])
+            {
+                return ((char *)&needle[i]);
+            }
+            j++;
+        }
+        i++;
+        j = 0;
+    }
+    return (NULL);
 }
 
-int main(void)
+/*int main(void)
 {
     char haystack[] = "Cavalcanti";
-    char needle[] = "val";
+    char needle[] = "42ti";
     size_t len;
 
-    len = 4;
-    char found = ft_strnstr(haystack, needle, len);
-    printf("Found:%c\n", found);
+    len = 15;
+    char *found = ft_strnstr(haystack, needle, len);
+    printf("Comparison:%s\n", found);
     return (0);
-}
+}*/
