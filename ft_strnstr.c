dcb8a343afc3223b6011e4fe1247f1e676a997b8
Ana Cavalcanti <87565077+anacavancalti@users.noreply.github.com>
2023-10-18 08:41:38 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,27 +5,25 @@
 /*                                                     +:+                    */
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/10/08 10:39:29 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/10/17 19:23:57 by anacavalcan   ########   odam.nl         */
+/*   Created: 2023/10/09 18:11:27 by anacaval      #+#    #+#                 */
+/*   Updated: 2023/10/18 08:36:36 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Cast to the address of a char in a string
-// locates a substring in a string
+// Locates a substring in a string
 // #include <stdio.h>
+#include "libft.h"
 
-#include <libft.h>
-
-
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t i;
     size_t j;
 
     i = 0;
     j = 0;
-    while (needle[i] != '\0')
-    {   
+    while(needle[i] != '\0')
+    {
         while(haystack[j] && j <= len)
         {
             if(haystack[j] == needle[i])
@@ -48,6 +46,7 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 
     len = 15;
     char *found = ft_strnstr(haystack, needle, len);
-    printf("Comparison:%s\n", found);
+    printf("Haystack:%s\n", haystack);
+    printf("Found:%s\n", found);
     return (0);
 }*/
