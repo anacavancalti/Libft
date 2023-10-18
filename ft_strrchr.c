708d431f9e026993531079d20f8bd575b6436f6e
Ana Cavalcanti <87565077+anacavancalti@users.noreply.github.com>
2023-10-18 14:31:48 +0200

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,31 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: anacaval <anacaval@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/05 12:30:49 by anacaval          #+#    #+#             */
-/*   Updated: 2023/10/05 14:48:49 by anacaval         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_strrchr.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/09 18:09:22 by anacaval      #+#    #+#                 */
+/*   Updated: 2023/10/18 14:30:54 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+// #include <stdio.h>
+#include "libft.h"
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-int i;
-
-i = 0;
-while (s[i] != '\0')
+	int	i;
+	char *found = (NULL);
+	    
+    i = 0;
+    while (s[i] != '\0')
     {
+        if(s[i] == c)
+        {
+            found = ((char *)s + i);
+        }
         i++;
     }
-while (s[i] > 0)
-    {
-        if (s[i] == c)
-        return ((char *)&s[i]);
-        i--;
-    }
-    return (NULL);
+    return (found);
 }
+
+/*int main(void)
+{
+    const char string[] = "Anacaval";
+    char to_find;
+	
+    to_find = 'a';
+	char *last_occurrence = ft_strrchr(string, to_find);
+    if(last_occurrence)
+    {
+        printf("Last Occurrence:%c\n%s", to_find, last_occurrence);
+    }
+    else
+    {
+        printf("Not Found:%c\n", to_find);
+    }
+    return (0);
+}*/
