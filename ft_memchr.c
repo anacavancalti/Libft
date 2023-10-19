abe51bccd20df3fd771c9ebff237ecbff0abe280
Ana Cavalcanti <87565077+anacavancalti@users.noreply.github.com>
2023-10-19 11:16:39 +0200

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,40 +6,40 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/18 14:34:24 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/10/18 15:21:17 by anacaval      ########   odam.nl         */
+/*   Updated: 2023/10/19 11:15:22 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-// #include "libft.h"
+// #include <stdio.h>
+#include "libft.h"
 
 void *ft_memchr(const void *s, int c, size_t n) 
 {
-    const unsigned char *str;
+    unsigned char *string;
     size_t i;
 
     i = 0;
-    str = ((const unsigned char *)s);
+    string = ((unsigned char *)s);
     while(i < n)
     {
-        if(str[i] == (unsigned char)c) // finds the first instance of c
+        if(string[i] == (unsigned char)c) 
         {
-            return ((void *)&str[i]); 
+            return (string + i); 
         }
         i++;
     }
     return (NULL);
 }
 
-int main(void)
+/*int main(void)
 {
     const char string[] = "Anacaval";
     char c;
     size_t bytes;
 
     c = 'c';
-    bytes = 3;
-    ft_memchr(string, c, bytes);
-    printf("Matching byte:%s\n", string);
+    bytes = 4;
+    char *found = ft_memchr(string, c, bytes);
+    printf("Matching byte:%s\n", found);
     return (0);
-}
+}*/
