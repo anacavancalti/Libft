60706c8b8e0e58b0d5211033dc56ee0fa4c0e148
Ana Cavalcanti <87565077+anacavancalti@users.noreply.github.com>
2023-10-20 08:36:00 +0200

    Add files via upload

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,15 +6,15 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 08:49:07 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/10/19 20:40:50 by anacavalcan   ########   odam.nl         */
+/*   Updated: 2023/10/20 08:34:45 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Allocates memory in a temporary array
 
-// #include <stdio.h>
-// #include <string.h>
-#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+// #include "libft.h"
 
 void *ft_memmove(void *dst, const void *src, size_t n)
 {
