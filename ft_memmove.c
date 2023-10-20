d6b94631feaf45eaf6953b3aa7a1af2f4a979ba6
Ana Cavalcanti <87565077+anacavancalti@users.noreply.github.com>
2023-10-20 11:33:01 +0200

    Add files via upload

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,48 +6,46 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 08:49:07 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/10/20 08:34:45 by anacaval      ########   odam.nl         */
+/*   Updated: 2023/10/20 11:31:43 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Allocates memory in a temporary array
 
-#include <stdio.h>
-#include <string.h>
-// #include "libft.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+#include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-    char *destination = dst;
-    const char *source = src;
-    char temporary[n];
-    size_t i;
-
-    i = 0;
-    while(i < n)
-    {
-        temporary[i] = source[i];
-        i++;
-    }
-    i = 0;
-    while(i < n)
-    {
-        destination[i] = temporary[i];
-        i++;
-    }
-    return (dst);
+	unsigned char	*temporary;
+	size_t			i;
+	
+	if (!dst && !src)
+	{
+		return (NULL);
+	}
+	i = 0;
+	temporary = ((unsigned char *)dst);
+	while (n--)
+	{
+		temporary[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	dst = ((void *)temporary);
+	return (dst);
 }
 
 /*int main(void)
 {
-    char destination[4];
-    char source[] = "Ana Cavalcanti";
-    size_t size;
+	char destination[4];
+	char source[] = "Anacaval";
+	size_t size;
 
-    size = sizeof(destination);
-    char *original = ft_memmove(destination, source, size); 
-    printf("Original:%s\n", original);
-    printf("Source:%s\n", source);
-    printf("Destination:%s\n", destination);
-    return (0);
+	size = 10;		
+	printf("Before:%s\n", destination);
+	ft_memmove(destination, source, size); 
+	printf("After:%s\n", destination);
+	return (0);
 }*/
