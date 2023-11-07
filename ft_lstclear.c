261b4f97c020acd7bf63821c0afb55f9f718a767
Ana Cavalcanti <anacaval@f0r3s24.codam.nl>
2023-11-07 12:15:03 +0100

    Added ft_lstclear.c function

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,8 +6,31 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/06 11:30:19 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/11/06 17:39:33 by anacaval      ########   odam.nl         */
+/*   Updated: 2023/11/07 12:13:54 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Deletes and frees the given node and every successor 
 
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list *given_node;
+	
+	if (!*lst)
+	{
+		return ;
+	}
+	if (!*del)
+	{
+		return ;
+	}
+	while (*lst)
+	{
+		given_node = *lst;
+		*lst = given_node->next;
+		ft_lstdelone(given_node, del);
+	}
+	*lst = NULL;
+}
