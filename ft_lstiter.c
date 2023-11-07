6df6d33814936ae739905e5f18c163e156c39cf0
Ana Cavalcanti <anacaval@f0r3s24.codam.nl>
2023-11-07 14:07:56 +0100

    Added new Bonus functions

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,28 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/06 11:30:24 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/11/06 11:30:25 by anacaval      ########   odam.nl         */
+/*   Updated: 2023/11/07 12:34:25 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*given_node;
+
+	given_node = lst;
+	if (!lst)
+	{
+		return ;
+	}
+	if (!f)
+	{
+		return ;
+	}
+	while (given_node)
+	{
+		f(given_node->content);
+		given_node = given_node->next;
+	}
+}
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,28 @@
 /*   By: anacaval <anacaval@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/06 11:30:29 by anacaval      #+#    #+#                 */
-/*   Updated: 2023/11/06 11:30:30 by anacaval      ########   odam.nl         */
+/*   Updated: 2023/11/07 14:07:27 by anacaval      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new_list;
+	t_list *given_node;
+	
+	new_list = NULL;
+	while (lst)
+	{
+		given_node = ft_lstnew(lst->content);
+		if (!given_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, given_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
